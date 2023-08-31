cf1616350f01001ed50d059a6acbb8c5e1c31c87
flypigmong <jseonghun85@gmail.com>
2023-08-31 21:57:05 +0900

    3장 게시판 만들고 새 글 작성하기 : Create :: 폼 데이터를 DTO로 받기 :: DTO를 데이터베이스에 저장하기 :: DB 데이터 조회하기

---
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -0,0 +1,29 @@
+package com.example.board.controller;
+
+import com.example.board.dto.ArticleForm;
+import com.example.board.entity.Article;
+import com.example.board.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+        System.out.println(form.toString());
+        //1 .DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        //2. 리파지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+        return "";
+    }
+}
