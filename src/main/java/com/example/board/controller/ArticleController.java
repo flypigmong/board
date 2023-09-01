9d5a2f89b668e808cd84c843555193c7dc1a8a71
flypigmong <jseonghun85@gmail.com>
2023-09-01 17:33:20 +0900

    5장 : 게시글 읽기 Read 5.1 :: 단일 데이터 조회하기

--- src/main/java/com/example/board/controller/ArticleController.java
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -7,8 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.hibernate.service.JavaServiceLoadable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -25,9 +29,21 @@ public class ArticleController {
         log.info(form.toString());
         //1 .DTO를 엔티티로 변환
         Article article = form.toEntity();
+        log.info(article.toString());
         //2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article" , articleEntity);
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 }
