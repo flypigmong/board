cf23946495cbaff84dc7492ac57ef859153ec852
flypigmong <jseonghun85@gmail.com>
2023-09-02 20:34:52 +0900

    7.3 ::: 수정 데이터를 DB에 갱신하기 : 데이터를 수정해 DB에 반영한 후 결과를 볼 수 있게 상세 페이지로 리다이렉트한다.

--- src/main/java/com/example/board/dto/ArticleForm.java
+++ src/main/java/com/example/board/dto/ArticleForm.java
@@ -7,11 +7,13 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class ArticleForm {
+    private Long id; // ID
     private String title; //제목
     private String content; //내용
 
 
     public Article toEntity() {
-        return new Article(null, title, content);
+
+        return new Article(id, title, content);
     }
 }
