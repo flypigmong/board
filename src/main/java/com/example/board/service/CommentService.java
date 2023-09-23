cda23773a79afd10c51696af9bc768139979d999
flypigmong <jseonghun85@gmail.com>
2023-09-23 18:16:08 +0900

    15장: 댓글 컨트롤러와 서비스 만들기 15.3 ::: 댓글 생성하기 15.4 ::: 댓글 수정하기

--- src/main/java/com/example/board/service/CommentService.java
+++ src/main/java/com/example/board/service/CommentService.java
@@ -1,10 +1,13 @@
 package com.example.board.service;
 
 import com.example.board.dto.CommentDto;
+import com.example.board.entity.Article;
 import com.example.board.entity.Comment;
 import com.example.board.repository.ArticleRepository;
 import com.example.board.repository.CommentRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.SmartLifecycle;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -29,5 +32,33 @@ public class CommentService {
         }
         // 3. 결과 반환
         return dtos;
+    }
+
+    @Transactional
+    public CommentDto create(long articleId, CommentDto dto) {
+        //1.게시글 조회 및 예외 발생
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! " +
+                                "대상 게시글이 없습니다."));
+        //2.댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto,article);
+        //3.댓글 엔티티를 DB에 저장
+        Comment created = commentRepository.save(comment);
+        //4.DTO로 변환해 반환
+        return CommentDto.createCommentDto(created);
+    }
+
+    @Transactional
+    public CommentDto update(Long id, CommentDto dto) {
+        //1.댓글 조회 및 예외 발생
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 수정 실패!" +
+                                "대상 댓글이 없습니다."));
+        //2.댓글 수정
+        target.patch(dto);
+        //3.DB로 갱신
+        Comment updated = commentRepository.save(target);
+        //4.댓글 엔티티를 DTO로 변환 및 반환
+        return CommentDto.createCommentDto(updated);
     }
 }
