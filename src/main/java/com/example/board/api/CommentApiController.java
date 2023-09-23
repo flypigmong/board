980002e27420570c2130d84fb71ca25e4b0c078b
flypigmong <jseonghun85@gmail.com>
2023-09-23 18:21:43 +0900

    15장: 댓글 컨트롤러와 서비스 만들기  15.6 댓글 삭제하기 :::

--- src/main/java/com/example/board/api/CommentApiController.java
+++ src/main/java/com/example/board/api/CommentApiController.java
@@ -1,6 +1,7 @@
 package com.example.board.api;
 
 import com.example.board.dto.CommentDto;
+import com.example.board.entity.Comment;
 import com.example.board.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -43,6 +44,12 @@ public class CommentApiController {
     }
 
 
-
     // 4.댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+        // 서비스에 위임
+        CommentDto deletedDto = commentService.delete(id);
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(deletedDto);
+    }
 }
